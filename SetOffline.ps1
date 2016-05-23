13049b7c0b1aef45c9712c6a4953a57f61ad3a89
Ben <BforBen@users.noreply.github.com>
2016-05-23 13:55:24 +0100

    Initial commit

---
+++ SetOffline.ps1
@@ -0,0 +1,8 @@
+﻿$sites = Get-Content -Path Sites.txt
+
+foreach($site in $sites)
+{
+    Write-Host Setting $site offline
+    $prefix = $site.Substring(2, ($site.IndexOf(".") - 2))
+    Copy-Item -Path app_offline.$prefix.htm -Destination $site\app_offline.htm
+}
---
+++ SetOnline.ps1
@@ -0,0 +1,8 @@
+﻿$sites = Get-Content -Path Sites.txt
+
+foreach($site in $sites)
+{
+    Write-Host Setting $site online
+    $prefix = $site.Substring(2, ($site.IndexOf(".") - 2))
+    Remove-Item -Path $site\app_offline.htm
+}
